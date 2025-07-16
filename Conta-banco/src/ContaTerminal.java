b2103fcd8486ed98593b4661f3fb37ed0aa7fbf2
Jônatas <jr.cesar2213@gmail.com>
2025-07-16 11:57:51 -0300

    Projetinho

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -3,14 +3,19 @@ public class ContaTerminal {
     public static void main(String[] args) {
        Scanner entrada = new Scanner(System.in); // Criei a classe Scanner para a entrada de dados
 
+       System.out.println("Olá, vamos fazer o seu cadastro no banco!");
+
        System.out.println("Por favor, digite o número Agência: ");
        int number = entrada.nextInt(); // O usuário digita no terminal
+
        System.out.println("Qual é a sua Agência: ");
        entrada.nextLine();  // Não deixa o \n do nextInt entrar no nextLine, não pula o nextLine
        String agencia = entrada.nextLine();
+
        System.out.println("Digite seu nome: ");
        String name = entrada.nextLine();
        name = name.toUpperCase(); // Deixa o nome do cliente em maiúsculo
+
        System.out.println("Digite o seu saldo: ");
        double saldo = entrada.nextDouble();
        entrada.close(); // Fecha o Scanner, boa prática
