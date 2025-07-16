ead55af0cc6504c41cdbe086e1f04383dee93403
Jônatas <jr.cesar2213@gmail.com>
2025-07-16 11:56:09 -0300

    Projetinho

--- Conta-banco/src/ContaTerminal.java
+++ Conta-banco/src/ContaTerminal.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-       
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in); // Criei a classe Scanner para a entrada de dados
+
+       System.out.println("Por favor, digite o número Agência: ");
+       int number = entrada.nextInt(); // O usuário digita no terminal
+       System.out.println("Qual é a sua Agência: ");
+       entrada.nextLine();  // Não deixa o \n do nextInt entrar no nextLine, não pula o nextLine
+       String agencia = entrada.nextLine();
+       System.out.println("Digite seu nome: ");
+       String name = entrada.nextLine();
+       name = name.toUpperCase(); // Deixa o nome do cliente em maiúsculo
+       System.out.println("Digite o seu saldo: ");
+       double saldo = entrada.nextDouble();
+       entrada.close(); // Fecha o Scanner, boa prática
+
+       System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", name, agencia, number, saldo);
     }
 }
